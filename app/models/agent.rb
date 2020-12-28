77269053ab6e4e9e85308516a9603aa28a809618
hzlhrdn <36228622+hzlhrdn@users.noreply.github.com>
2020-12-28 15:36:54 +0800

    Update agent.rb

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -3,7 +3,7 @@ require 'csv'
 class Agent < ApplicationRecord
 	include GenerateCsv
 
-	belongs_to :user
+	belongs_to :user, optional: true
 	has_many :spouses,  dependent: :destroy
 	has_many :dependents,dependent: :destroy
 	belongs_to :plan
